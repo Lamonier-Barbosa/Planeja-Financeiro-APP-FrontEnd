1dfe154b4b0b4013bdc48b44494ebff2fed8f4cc
Lamonier Barbosa <lamoni_lr@hotmail.com>
2026-09-21 20:30:12 -0300

    adicionando component cartao

---
+++ src/app/cartoes/cadastro-cartao/cadastro-cartao.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-cartao',
+  imports: [],
+  templateUrl: './cadastro-cartao.html',
+  styleUrl: './cadastro-cartao.scss',
+})
+export class CadastroCartao {}
